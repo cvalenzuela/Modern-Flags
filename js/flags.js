4ad012e0c212d969be929e3231630721484fd385
Cristobal Valenzuela <cvalenzuela@nyu.edu>
2016-10-03 17:26:54 -0400

    add css animations

--- js/flags.js
+++ js/flags.js
@@ -1,4 +1,4 @@
-var data, countryDescriptionTag, countryGiniTag, countryHdiTag, countryPopulationTag, countryName, selectedCountry, previousCountry,hdiLowest,hdiHighiest, giniLowest, giniHighiest, countryHdi, showCountryHdi, countryGini, countryPopulation, highiestPopulation, lowestPopulation, flagPoleY,startOfY,startOfX, canvasTag;
+var data, countryDescriptionTag, countryGiniTag, countryHdiTag, countryPopulationTag, countryName, selectedCountry, previousCountry,hdiLowest,hdiHighiest, giniLowest, giniHighiest, countryHdi, showCountryHdi, countryGini, countryPopulation, highiestPopulation, lowestPopulation, flagPoleY,startOfY,startOfX, canvasTag, hdiDescription, giniDescription, populationDescription;
 
  /* ---- Look for selected country in ul>li or for Random Value in JSON ---- */
 if (document.addEventListener){
@@ -34,12 +34,20 @@ function setup(){
     noStroke();
     startOfY = 0;
     
-    /* ---- Create Dynamic Elements  ---- */
+    /* ---- Create Static and Dynamic Tags  ---- */
     countryName = createElement('h6', " ");
     countryHdi = createElement('p', " ");
     countryGini = createElement('p', " ");
     countryPopulation = createElement('p', " ");
     showCountryHdi = createElement('p', " ");
+    hdiDescription = createElement('p', "A country scores higher Human Development Index (HDI) when the lifespan is higher, the education level is higher, the GDP per capita is higher, the fertility rate is lower, and the inflation rate is lower.");
+    giniDescription = createElement('p', "The Gini coefficient is a measure of inequality of a distribution. It is defined as a ratio with values between 0 and 1.  A Gini coefficient of 100% expresses maximal inequality.");
+    populationDescription = createElement('p', "All the inhabitants of a particular country as in 2016.")
+    
+    /* ---- Hide all Hoverable Text ---- */
+    hdiDescription.hide();
+    giniDescription.hide();
+    populationDescription.hide();
     
     /* ---- Create variables for ID Tags  ---- */
     countryDescriptionTag = document.getElementById('countryDescription');
@@ -65,17 +73,18 @@ function draw() {
     noStroke();
     fill('#EEEEEE');
     rect(startOfX,0,6,windowHeight); // Draw Flagpole
+    rect(startOfX-16,475,40,20);
     
     /* ---- Check if the country selected is valid---- */
     if(selectedCountry != undefined && previousCountry != selectedCountry){
-        
+
         /* ---- Remove previous country ---- */
         resetCanvas();
         
         /* ---- Create new country ---- */
         countryName = createElement('h6', selectedCountry); 
-        countryHdi = createElement('p', '<b>Human Development Index:</b> ' + data[selectedCountry].hdi);
-        countryGini = createElement('p', '<b>Gini Coefficient:</b> ' + data[selectedCountry].gini);
+        countryHdi = createElement('p', '<b>HDI:</b> ' + data[selectedCountry].hdi);
+        countryGini = createElement('p', '<b>Gini Coefficient:</b> ' + data[selectedCountry].gini + '%');
         countryPopulation = createElement('p', '<b>Population:</b> ' + data[selectedCountry].population + '000');
         showCountryHdi = createElement('p', '<b>HDI </b>' + data[selectedCountry].hdi);
         
@@ -85,7 +94,10 @@ function draw() {
         countryGini.parent(countryGiniTag);
         countryPopulation.parent(countryPopulationTag);
         showCountryHdi.parent(canvasTag);
-
+        hdiDescription.parent(indexDescription);
+        giniDescription.parent(indexDescription);
+        populationDescription.parent(indexDescription);
+        
         /* ---- Create new variables to draw  ---- */
         var hdi = map(data[selectedCountry].hdi,hdiLowest,hdiHighiest,windowHeight/2+250,startOfY);
         var gini = int(map(data[selectedCountry].gini,giniLowest,giniHighiest,0,width));
@@ -96,17 +108,17 @@ function draw() {
         rect(startOfX,hdi,population,population-population/3); // Draw the flag
         stroke('#aaaaaa');
         line(startOfX - 20,hdi,startOfX-5,hdi);  // Draw HDI Line
-        showCountryHdi.position(startOfX - 55,hdi-5);
+        showCountryHdi.position(startOfX - 55,hdi-5); // Show HDI next to flag
         previousCountry = selectedCountry;
-        println(hdi);
         
     }
-
+    
 }
 
 /* ---- Watch for rezing of the screen ---- */
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight/2+100);
+    resetCanvas();
 }
 
 /* ---- Reset Canvas when needed ---- */
@@ -118,3 +130,25 @@ function resetCanvas(){
     countryPopulation.remove();
     showCountryHdi.remove();
 }
+
+/* ---- Show description when hover over them ---- */
+function showWhenHover(element){
+    element.addClass('leftInfoBox');
+    element.removeClass('fadeOutLeft');
+    element.addClass('animated');
+    element.addClass('fadeInLeft');
+    element.show();    
+}
+
+/* ---- Hide description when not hover over them ---- */
+function hideWhenNoHover(element){
+    element.removeClass('fadeInLeft');
+    element.addClass('fadeOutLeft');
+    element.hide();
+    
+}
+
+/* ---- Cursor Styles---- */
+document.getElementById("hdi").style.cursor = "pointer";
+document.getElementById("gini").style.cursor = "pointer";
+document.getElementById("population").style.cursor = "pointer";
